864b4313931db5c3a8c813f5a4ba17c7c23fd462
Shakil Ahmed Atik <shakilatik@gmail.com>
2021-04-02 13:05:13 +0600

    Added Category & Product Section

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link, withRouter } from 'react-router-dom'
 import { signout, isAuthenticated } from '../auth'
 
@@ -19,8 +19,32 @@ const Menu = ({ history }) => (
         </Link>
       </li>
 
+      {isAuthenticated() && isAuthenticated().user.role === 0 && (
+        <li className='nav-item'>
+          <Link
+            className='nav-link'
+            style={isActive(history, '/user/dashboard')}
+            to='/user/dashboard'
+          >
+            Dashboard
+          </Link>
+        </li>
+      )}
+
+      {isAuthenticated() && isAuthenticated().user.role === 1 && (
+        <li className='nav-item'>
+          <Link
+            className='nav-link'
+            style={isActive(history, '/admin/dashboard')}
+            to='/admin/dashboard'
+          >
+            Dashboard
+          </Link>
+        </li>
+      )}
+
       {!isAuthenticated() && (
-        <>
+        <Fragment>
           <li className='nav-item'>
             <Link
               className='nav-link'
@@ -40,7 +64,7 @@ const Menu = ({ history }) => (
               Signup
             </Link>
           </li>
-        </>
+        </Fragment>
       )}
 
       {isAuthenticated() && (
